789c7a14897cb718569c02595ddb9c5bb3b13410
Joohwi Lee <jlee@autox.ai>
2019-03-28 13:56:46 -0700

    Prettier

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,7 +3,4 @@ import Greeting from "./popup/greeting_component.jsx";
 import React from "react";
 import { render } from "react-dom";
 
-render(
-  <Greeting/>,
-  window.document.getElementById("app-container")
-);
+render(<Greeting />, window.document.getElementById("app-container"));
